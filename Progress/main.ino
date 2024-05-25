e451e86e70e7b0b99e631f316c7d4504ab1c7ebe
wn-l1x <144139330+wn-l1x@users.noreply.github.com>
2024-05-25 12:49:13 +0700

    Sistem ADC, Servo dengan interrupt don
    sedang menambahkan fitur ngecek

--- Progress/main.ino
+++ Progress/main.ino
@@ -2,14 +2,21 @@ extern "C"{
   void init();
   void adc_loop();
   void servo();
+  void myDelay();
 }
+
+ISR(INT0_vect){
+  PORTB |= (1 << 0); // Set PORTB0 to high
+
+}
+
 void setup() {
   init();
 }
 
 void loop() {
   adc_loop();
-  /* call if interrupt
   servo();
-  */
 }
+
+
